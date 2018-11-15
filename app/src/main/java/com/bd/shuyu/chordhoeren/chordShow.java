97a8ff7b910bca7fcee2c6d4e2e7365df5b4737e
seanchenye <seanchenye1997@gmail.com>
2018-11-15 01:13:14 -0500

    20181115

---
+++ app/src/main/java/com/bd/shuyu/chordhoeren/chordShow.java
@@ -0,0 +1,13 @@
+package com.bd.shuyu.chordhoeren;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class chordShow extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chord_show);
+    }
+}
